5057e474fa14a5cd968e5823895683cda9702e8d
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2018-09-15 16:59:36 +0200

    Added code for hiding tabs for certain user groups

--- App_Code/EditorModelEvents.cs
+++ App_Code/EditorModelEvents.cs
@@ -1,28 +1,33 @@
 ﻿using System.Linq;
 
 using Umbraco.Core;
+using Umbraco.Core.Models.Membership;
+using Umbraco.Web;
 using Umbraco.Web.Editors;
 using Umbraco.Web.Models.ContentEditing;
 
 public class EditorModelEvents : ApplicationEventHandler
 {
     protected override void ApplicationStarted(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
-    {
+    {       
         EditorModelEventManager.SendingContentModel += this.EditorModelEventManager_SendingContentModel;
     }
 
     private void EditorModelEventManager_SendingContentModel(System.Web.Http.Filters.HttpActionExecutedContext sender, EditorModelEventArgs<Umbraco.Web.Models.ContentEditing.ContentItemDisplay> e)
     {
-        var contentItemDisplay = e.Model;
+        var contentItemDisplay = e.Model;           
 
         DisablePreview(contentItemDisplay);
 
         ChangeTabNames(contentItemDisplay);
+
+        HideTabs(contentItemDisplay);
     }
+
 
     private void ChangeTabNames(ContentItemDisplay contentItemDisplay)
     {
-        var doctypes = new  [] { "products", "blog", "people" };
+        var doctypes = new[] { "products", "blog", "people" };
 
         if (doctypes.Contains(contentItemDisplay.ContentTypeAlias))
         {
@@ -39,6 +44,16 @@ public class EditorModelEvents : ApplicationEventHandler
         {
             contentItemDisplay.AllowPreview = false;
             contentItemDisplay.Urls = new string[0];
+        }
+    }
+
+    private void HideTabs(ContentItemDisplay contentItemDisplay)
+    {
+        var usergroups = UmbracoContext.Current.Security.CurrentUser.Groups.ToList();
+
+        if (usergroups.Exists(x => x.Name == "Writers"))
+        {
+            contentItemDisplay.Tabs = contentItemDisplay.Tabs.Where(x => x.Label != "Navigation & SEO");
         }
     }
 }
