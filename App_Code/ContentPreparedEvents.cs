0d9fc91eed5ff7da992b7ca8e351f9740b635223
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2018-09-25 17:22:16 +0200

    Added example of content event prepared event

---
+++ App_Code/ContentPreparedEvents.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+using Umbraco.Core;
+using Umbraco.Web.PublishedContentModels;
+using Umbraco.Web.Routing;
+
+/// <summary>
+/// Summary description for ContentPreparedEventes
+/// </summary>
+public class ContentPreparedEvents : ApplicationEventHandler
+{
+    protected override void ApplicationStarted(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
+    {
+        PublishedContentRequest.Prepared += PublishedContentRequestOnPrepared;
+    }
+
+    private void PublishedContentRequestOnPrepared(object sender, EventArgs eventArgs)
+    {
+        var request = sender as PublishedContentRequest;
+
+        if (request == null || !request.HasPublishedContent)
+            return;
+
+        if (request.PublishedContent.DocumentTypeAlias == Home.ModelTypeAlias)
+        {
+            request.TrySetTemplate("HomeClean");
+        }
+    }
+}
