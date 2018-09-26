1ad2c03a4aaf68e0b28cb87b7b2b219e96415684
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2018-09-26 09:04:18 +0200

    Added url segment provider for order status page

---
+++ App_Code/OrderStatusUrlSegmentProvider.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+
+using Umbraco.Core.Models;
+using Umbraco.Core.Strings;
+using Umbraco.Web.PublishedContentModels;
+
+using IContentBase = Umbraco.Core.Models.IContentBase;
+
+/// <summary>
+/// Summary description for OrderStatusUrlSegmentProvider
+/// </summary>
+public class OrderStatusUrlSegmentProvider : IUrlSegmentProvider
+{
+    /// <summary>Gets the default url segment for a specified content.</summary>
+    /// <param name="content">The content.</param>
+    /// <returns>The url segment.</returns>
+    public string GetUrlSegment(IContentBase content)
+    {
+        if (content.GetContentType().Alias == OrderStatus.ModelTypeAlias)
+        {
+            return "orderstatus";
+        }
+
+        return null;
+    }
+
+    /// <summary>
+    /// Gets the url segment for a specified content and culture.
+    /// </summary>
+    /// <param name="content">The content.</param>
+    /// <param name="culture">The culture.</param>
+    /// <returns>The url segment.</returns>
+    /// <remarks>This is for when Umbraco is capable of managing more than one url
+    /// per content, in 1-to-1 multilingual configurations. Then there would be one
+    /// url per culture.</remarks>
+    public string GetUrlSegment(IContentBase content, CultureInfo culture)
+    {
+        return GetUrlSegment(content);
+    }
+}
---
+++ App_Code/VirtualRouteEvents.cs
@@ -0,0 +1,11 @@
+﻿using Umbraco.Core;
+using Umbraco.Core.Strings;
+
+public class VirtualRouteEvents : ApplicationEventHandler
+{
+   
+    protected override void ApplicationStarting(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
+    {
+        UrlSegmentProviderResolver.Current.InsertTypeBefore<DefaultUrlSegmentProvider, OrderStatusUrlSegmentProvider>();
+    }
+}
