a2939499133306d757e139900c1d36e8295436bf
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2018-09-27 16:28:28 +0200

    Setup virtual route for order status

---
+++ App_Code/OrderStatusController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+using Umbraco.Web.Models;
+using Umbraco.Web.Mvc;
+
+public class OrderStatusController : RenderMvcController
+{
+    public ActionResult OrderStatus(RenderModel model, string orderId)
+    {      
+        return this.View("OrderStatus", model: model);
+    }
+}
--- App_Code/VirtualRouteEvents.cs
+++ App_Code/VirtualRouteEvents.cs
@@ -1,5 +1,11 @@
-﻿using Umbraco.Core;
+﻿using System;
+using System.Web.Routing;
+
+using Umbraco.Core;
 using Umbraco.Core.Strings;
+using Umbraco.Web;
+using Umbraco.Web.Mvc;
+using Umbraco.Web.PublishedContentModels;
 
 public class VirtualRouteEvents : ApplicationEventHandler
 {
@@ -7,5 +13,28 @@ public class VirtualRouteEvents : ApplicationEventHandler
     protected override void ApplicationStarting(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
     {
         UrlSegmentProviderResolver.Current.InsertTypeBefore<DefaultUrlSegmentProvider, OrderStatusUrlSegmentProvider>();
+    }
+    
+    protected override void ApplicationStarted(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
+    {
+        var orderStatusPage =
+            UmbracoContext.Current.ContentCache.GetSingleByXPath(string.Format("//{0}[@isDoc]", OrderStatus.ModelTypeAlias)).OfType<OrderStatus>();
+
+        if (orderStatusPage != null)
+        {
+            RouteTable.Routes.MapUmbracoRoute(
+                "order_status",
+                this.GetRoutePathFromNodeUrl(orderStatusPage.Url).EnsureEndsWith("/") + "{orderId}",
+                new { controller = "OrderStatus", action = "OrderStatus" },
+                new UmbracoVirtualNodeByIdRouteHandler(orderStatusPage.Id));
+        }
+    }
+
+    private  string GetRoutePathFromNodeUrl(string routePath)
+    {
+        Uri result;
+        return Uri.TryCreate(routePath, UriKind.Absolute, out result)
+                   ? result.LocalPath.TrimStart('/')
+                   : routePath.TrimStart('/');
     }
 }
